b093ec19676483ed21075e735e0fe08e2ed67a41
Suraram Pimankham <76487588+SuraramPimankham@users.noreply.github.com>
2022-09-18 21:09:15 +0700

    update work laravel lap

---
+++ work/web_test/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Models\Profile;
+
+class ProfileController extends Controller
+{
+    public function saveProfile(Request $req)
+    {
+        
+    }
+}
---
+++ work/web_test/resources/views/page.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>ยินดีต้องรับสู่หน้าต่างของฉัน</h2>
+
+@endsection
